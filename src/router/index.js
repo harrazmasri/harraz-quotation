144915ac595e942fccd82134a9b047ee50707d54
harrazmasri <harraz@rekasawang.com>
2024-05-31 01:24:54 +0800

    base path fix

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/harraz-quotation',
+      path: '/',
       name: 'home',
       component: HomeView
     },
