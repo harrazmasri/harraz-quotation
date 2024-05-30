e560eb31c5ca191bc130600f067d048e0615322b
harrazmasri <harraz@rekasawang.com>
2024-05-31 01:43:41 +0800

    idk

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/harraz-quotation/',
+  base: '/harraz-quotation',
   plugins: [
     vue(),
   ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+  // resolve: {
+  //   alias: {
+  //     '@': fileURLToPath(new URL('./src', import.meta.url))
+  //   }
+  // }
 })
