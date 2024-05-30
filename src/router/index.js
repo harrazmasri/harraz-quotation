a6cd06e86da521df7a1827c73d16946657dddd5a
harrazmasri <harraz@rekasawang.com>
2024-05-31 02:16:09 +0800

    idddk

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
-      path: '/harrazmasri/',
+      path: '/harraz-quotation/',
       name: 'home',
       component: HomeView
     },
